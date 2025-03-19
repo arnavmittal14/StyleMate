e911c6762dc4f70b19e7bc647634f7c537f5c435
DeboDam <Debojeetdam.business@gmail.com>
2025-03-18 20:37:20 -0600

    Fixed logout and Navbar

--- stylemate/frontend/src/components/navbar/Navbar.jsx
+++ stylemate/frontend/src/components/navbar/Navbar.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../pages/login/AuthContext.jsx"; 
-import './Navbar.css'; 
+import './Navbar.css';
 
 export default function Navbar() {
 
-  const { logout } = useAuth(); // Get logout function from AuthContext
+  // const { logout } = useAuth(); // Get logout function from AuthContext
   const navigate = useNavigate(); // For redirection
 
   const handleLogout = () => {
-    logout(); // Reset authentication
+    localStorage.clear();
     navigate("/login"); // Redirect to login page
   };
+
+  const isGuestUser = () => {
+    const user = localStorage.getItem('user');
+    const userJSON = JSON.parse(user);
+    if (userJSON?.email && userJSON?.email?.includes("guest")) {
+      return true;
+    }
+
+    return false;
+  }
 
   return (
     <nav className="navbar">
@@ -20,7 +29,7 @@ export default function Navbar() {
         <a href="/" className="nav-link">Home</a>
         <a href="/Closet" className="nav-link">Closet</a>
         <a href="/Favorites" className="nav-link">Favorites</a>
-        <a href="/Profile" className="nav-link">Profile</a>
+        {!isGuestUser() && <a href="/Profile" className="nav-link">Profile</a>}
         <button className="logout-button" onClick={handleLogout}>Logout</button>
       </nav>
     </nav>
