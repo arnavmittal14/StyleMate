3f93fb8f0f756e1e556a0c18b3c48892c66dae14
arnavmittal14 <arnavmittal14@outlook.com>
2025-03-20 09:26:47 -0600

    Login fix

--- stylemate/frontend/src/api.js
+++ stylemate/frontend/src/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const apiUrl = "/choreo-apis/stylemate/app/v1";
+console.log("Using API URL:", apiUrl);
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
-})
+    baseURL: apiUrl,
+    withCredentials: true, 
+});
+
 export { api, apiUrl };
