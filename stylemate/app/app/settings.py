38bf57aa13ff837c94aea8c367968e8bc64f0791
arnavmittal14 <arnavmittal14@outlook.com>
2025-03-20 02:49:33 -0600

    Updated Procfile, endpoints.yaml, and settings.py

--- stylemate/app/app/settings.py
+++ stylemate/app/app/settings.py
@@ -98,12 +98,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "stylemate_database",
-        "USER": "root",
-        "PASSWORD": "P@$$w0rd123",
-        "HOST": "127.0.0.1",  # Use 127.0.0.1 instead of localhost
-        "PORT": "3306",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
